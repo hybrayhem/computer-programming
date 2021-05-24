94b95463edba1779a3561850bf4794c5ffe7c2b3
hybrayhem <halil000ibrahim@gmail.com>
2021-05-24 22:49:31 +0300

    10/dao working but i will switch to global

--- dynamic_array_of_pointers.c
+++ dynamic_array_of_pointers.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int numerate_genre(char **genres, char *genre){
+int numerate_genre(char *genres[], char genre[]){
 	int i, j, flag =0;
 	char **temp = NULL;
 	printf("genre = %s\n", genre);
@@ -20,40 +20,30 @@ int numerate_genre(char **genres, char *genre){
 	else{
         printf("Not found.\n");
 
-        genres = (char **)calloc(i+2, sizeof(char));
-        
-        for(j = 0; j < i; j++){
-            genres[j] = temp[j];
+		temp = (char **)calloc(i, sizeof(char));
+		for(j = 0; j < i; j++){
+			temp[j] = (char *)malloc((strlen(genres[j])+1)*sizeof(char));
+			strcpy(temp[j], genres[j]);
         }
-        genres[i] = genre;
-        genres[i+1] = NULL;
-        
-        if(temp != NULL) free(temp);
+    	for(i=0; temp[i] != NULL; i++){
+			printf("*temp %d. = %s\n", i, temp[i]);
+		}
 
-        temp = genres;
 
-    }
-	
-	/*if(!flag){
-		printf("save genre to genres[%d], genres[%d] = NULL\n\n", i, i+1);
-		temp = genres;
-		
 		genres = (char **)calloc(i+2, sizeof(char));
-		
-		for(i=0; temp[i] != NULL; i++){
-		printf("temp %d. = %s\n", i, temp[i]);
-		genres[i] = temp[i];
-	}
-		
-		genres[i] = genre;
-		genres[i+1] = NULL;
-	}*/
+        for(j = 0; j < i; j++){
+			genres[j] = (char *)malloc((strlen(temp[j])+1)*sizeof(char));
+			strcpy(genres[j], temp[j]);
+        }
+		genres[i] = (char *)malloc((strlen(genre)+1)*sizeof(char));
+		strcpy(genres[i], genre);
+        genres[i+1] = NULL;
 
+		free(temp);
+	}
+	
     for(i=0; genres[i] != NULL; i++){
 		printf("*genres %d. = %s\n", i, genres[i]);
-		if(strcmp(genres[i], genre) == 0){
-			flag = 1;			
-		}
 	}
 	printf("\n\n");
     
@@ -62,15 +52,24 @@ int numerate_genre(char **genres, char *genre){
 
 int main()
 {
+	int i;
 	char **genres;
-    genres = (char **)calloc(1, sizeof(char));
-    genres[0] = NULL;
-	/*genres[0] = "action";
+    /*genres = (char **)calloc(1, sizeof(char));
+    genres[0] = NULL;*/
+	genres = (char **)calloc(3, sizeof(char));
+	genres[0] = "action";
 	genres[1] = "dram";
-	genres[2] = NULL;*/
+	genres[2] = NULL;
 	numerate_genre(genres, "dram");
 	numerate_genre(genres, "comedy");
+	for(i=0; genres[i] != NULL; i++){
+		printf("#genres %d. = %s\n", i, genres[i]);
+	}
+	printf("\n");
 	numerate_genre(genres, "action");
+	/*numerate_genre(genres, "dram");
+	numerate_genre(genres, "foreign");
+	numerate_genre(genres, "dram");*/
 	free(genres);
 	return 0;
 }
